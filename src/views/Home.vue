7a414bf3fd1b7f8781ab48d0b189f3c88707dd82
Austin Chermack <achermack@wisc.edu>
2020-06-16 23:26:16 -0500

    projects work/prep for contact

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,22 @@
         </v-container>
         <v-container id="projects" mt-5>
           <v-layout wrap>
-            <span class="display-1 font-weight-thin">Projects</span>           
+            <span class="display-1 font-weight-thin">Projects</span>
+            <v-flex xs12 class="mt-5">
+              <v-layout wrap>
+                <template v-for="(app, i) in apps">
+                  <v-flex :key="i" xs12 sm6 md4>
+                    <p-portfolio-app :app="app" />
+                  </v-flex>
+                </template>
+              </v-layout>
+            </v-flex>          
+          </v-layout>
+        </v-container>
+        <v-container id="Contact">
+          <v-layout wrap>
+            <span class="display-1 font-weight-thin">Contact</span>
+            
           </v-layout>
         </v-container>
       </v-layout>
@@ -28,16 +43,56 @@
 
 <script>
 // @ is an alias to /src
+import PPortfolioApp from "@/components/PPortfolioApp.vue";
 
 export default {
   name: 'Home',
   components: {
-
+    PPortfolioApp
   },
  data() {
     return {
       aboutText: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
       resumeLink: "https://github.com/achermack",
+      
+      apps: [
+        {
+          name: "Portfolio Site",
+          logo: "logo.png",
+          link: "https://achermack.github.io",
+
+          description:
+            "I created this website to showcase some of my work as a Software Developer. This website is built using the Vue.js and Vuetify and leverages Travis CI and Github Pages for automatic building and deployment. Check out the Github Repository below!",
+          technologies: [
+            {
+              name: "Vue",
+              icon: "mdi-vuejs",
+              link: "https://vuejs.org"
+            },
+            {
+              name: "Vuetify",
+              icon: "mdi-vuetify",
+              link: "https://vuetifyjs.com/en/"
+            },
+            {
+              name: "Travis-CI",
+              icon: "mdi-wrench",
+              link: "https://travis-ci.org/"
+            },
+            {
+              name: "Github Pages",
+              icon: "mdi-github",
+              link: "https://pages.github.com"
+            }
+          ],
+          code: {
+            name: "Github",
+            icon: "mdi-github",
+            link: "https://www.github.com/achermack"
+          }
+        }
+      ],
+      
       contactLinks: [
         {
           name: "Email",
