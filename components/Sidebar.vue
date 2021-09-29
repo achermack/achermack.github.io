e7217ce4a53f053ad4fe9bb3205ae41f73f28461
Austin <achermack@wisc.edu>
2021-09-28 22:00:08 -0500

    Sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-navigation-drawer absolute permanent left color="transparent">
+    <v-list>
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Table of Contents
+          </v-list-item-title>
+          <v-list-item-subtitle />
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-icon>mdi-menu-down</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+      <v-divider />
+      <v-list-item
+        v-for="link of project.toc"
+        :key="link.id"
+        link
+        :class="{ 'ml-4': link.depth === 3 }"
+        class="toc-list"
+        :to="`#${link.id}`"
+      >
+        <v-list-item-content> {{ link.text }} </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    project: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-container>
-    <article>
-      <nuxt-content :document="project" />
-    </article>
+  <v-container fill-height fluid>
+    <Sidebar v-if="$device.isDesktop" :project="project" />
+    <v-row justify="center">
+      <article class="ml-3">
+        <nuxt-content :document="project" />
+      </article>
+    </v-row>
   </v-container>
 </template>
 
@@ -12,6 +15,41 @@ export default {
     const project = await $content('projects', params.slug).fetch()
 
     return { project }
+  },
+  data () {
+    return {
+      currentlyActiveToc: '',
+      observer: null,
+      observerOptions: {
+        root: this.$refs.nuxtContent,
+        threshold: 0
+      }
+    }
+  },
+  mounted () {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute('id')
+        if (entry.isIntersecting) {
+          this.currentlyActiveToc = id
+        }
+      })
+    }, this.observerOptions)
+
+    // Track all sections that have an `id` applied
+    document
+      .querySelectorAll('.nuxt-content h2[id], .nuxt-content h3[id]')
+      .forEach((section) => {
+        this.observer.observe(section)
+      })
+  },
+  beforeDestroy () {
+    this.observer.disconnect()
   }
 }
 </script>
+<style>
+aside {
+  float: left;
+}
+</style>
